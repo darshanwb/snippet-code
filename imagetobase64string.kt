e0383bd6918bfed00cb8bdd9b9989731982797b3
darshanwb <124980776+darshanwb@users.noreply.github.com>
2023-02-10 11:38:36 +0530

    Create imagetobase64string.kt

---
+++ imagetobase64string.kt
@@ -0,0 +1,14 @@
+//  Here's a reusable function that converts an image to a Base64 string in Kotlin
+
+/*
+In this code, the input bitmap is first compressed into a ByteArrayOutputStream using the compress method. 
+Then, the toByteArray method is used to convert the ByteArrayOutputStream to a byteArray. 
+And Finally, the Base64.encodeToString method is used to encode the byteArray into a Base64 string, which is returned as the function's result.
+*/
+
+fun imageToBase64(bitmap: Bitmap): String {
+    val byteOutputStream = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteOutputStream)
+    val byteArray = byteOutputStream.toByteArray()
+    return Base64.encodeToString(byteArray, Base64.DEFAULT)
+}
