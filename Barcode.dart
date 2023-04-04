3176d4712dc9b805a46afe912e1fd44a252a9b45
darshanwb <124980776+darshanwb@users.noreply.github.com>
2023-04-04 15:13:40 +0530

    Create Barcode.dart
    This code is a method that uses the FlutterBarcodeScanner package in Flutter to scan a barcode and then navigate to a book details screen with the scanned barcode value.

---
+++ Barcode.dart
@@ -0,0 +1,28 @@
+Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+
+    print("ISBN of Book: $_scanBarcode");
+    showToast("ISBN: $_scanBarcode");
+
+    Navigator.pushNamed(context, AppRoutes.bookDetail,
+        arguments: BookDetails(
+          bookIsbn: _scanBarcode,
+        ));
+  }
