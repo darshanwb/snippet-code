e6fb470b1e0ede58f34fca5ce203d814e1a1755c
darshanwb <124980776+darshanwb@users.noreply.github.com>
2023-04-21 18:06:30 +0530

    LoadingDialog
    The showDialog method is used to show a dialog box on top of the current screen. The first parameter context is required and is the context of the current widget. The barrierDismissible parameter is optional and specifies whether the user can dismiss the dialog box by tapping outside of it. If it is set to false, the user cannot dismiss the dialog box until the operation that is being performed is complete.

---
+++ LoadingDialog.dart
@@ -0,0 +1,10 @@
+showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return Center(child: CircularProgressIndicator());
+        },
+      );
+
+///For Stop
+Navigator.pop(context);
